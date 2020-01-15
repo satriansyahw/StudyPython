411e3d6ad67ecbce70200e6f8d29f4b49fd973d0
satriansyahw <satriansyahw@gmail.com>
2020-01-15 11:18:30 +0700

    test3.py
    test3.py

--- Test2.py
+++ Test2.py
@@ -1,3 +1,4 @@
+# string
 name="kiran"
 kar1=name[1]
 print(kar1)
@@ -14,5 +15,34 @@ print('Split 0 : '+txtsplit[0])
 print('Split 1 : '+txtsplit[1])
 print('Split 2 : '+txtsplit[2])
 
-mylist=["apple","orange","grape"]
+mylist=["apple","orange","grape",234,98.67]
+mylist[2]=88
+
+print(mylist[0])
 print(mylist[2])
+print(mylist[4])
+print(mylist)
+mylist.append("34")
+print(mylist)
+
+#Dictionary object
+
+data ={"p1":"Yayan","p2":"Bintang","p3":"Ara","p4":"Kiran"}
+print(data)
+print(data['p3'])
+
+
+myurl =  "sixty-north.com/c/t.txt"
+from urllib.request import urlopen
+
+with urlopen('http://sixty-north.com/c/t.txt') as story:
+    story_words=[]
+    for line in story:
+        line_world=line.split()
+        for world in line_world:
+            story_words.append(world)
+            
+            print(story_words)
+  
+
+
