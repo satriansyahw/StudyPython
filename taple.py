9e03ed845ea5e86e688bb607623daa3fbd0a689e
satriansyahw <satriansyahw@gmail.com>
2020-01-15 13:40:15 +0700

    tapleupdate
    tapleupdate

--- taple.py
+++ taple.py
@@ -5,3 +5,18 @@ print(t[1])
 hasil = t+p
 print(hasil)
 print(hasil[3])
+tuple2d=(("a","b"),(1,2),(5,"B"))
+print(tuple2d)
+print(tuple2d[2])
+print(tuple2d[2][0])
+print(tuple2d[2][1])
+
+
+tuple3d=(("a","b","c"),(1,2,"C"),(5,"B",1.2))
+print(tuple3d)
+print(tuple3d[2])
+print(tuple3d[2][0])
+print(tuple3d[2][1])
+print(tuple3d[2][2])
+
+
